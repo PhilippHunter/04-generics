43a12d409a29d1c72654c1d2f67b2a078fab32cf
PhilippHunter <philippjaeger98@web.de>
2019-04-11 19:59:02 +0200

    added initial test body for testing map function

--- src/test/java/ohm/softa/a04/tests/SimpleListTests.java
+++ src/test/java/ohm/softa/a04/tests/SimpleListTests.java
@@ -17,58 +17,64 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 public class SimpleListTests {
 
-	private final Logger logger = LogManager.getLogger();
-	private SimpleList<Integer> testList;
+  private final Logger logger = LogManager.getLogger();
+  private SimpleList<Integer> testList;
 
-	@BeforeEach
-	void setup(){
-		testList = new SimpleListImpl<>();
+  @BeforeEach
+  void setup() {
+    testList = new SimpleListImpl<>();
 
-		testList.add(1);
-		testList.add(2);
-		testList.add(3);
-		testList.add(4);
-		testList.add(5);
-	}
+    testList.add(1);
+    testList.add(2);
+    testList.add(3);
+    testList.add(4);
+    testList.add(5);
+  }
 
-	@Test
-	void testAddElements(){
-		logger.info("Testing if adding and iterating elements is implemented correctly");
-		int counter = 0;
-		for(int i : testList){
-			counter++;
-		}
-		assertEquals(5, counter);
-	}
+  @Test
+  void testAddElements() {
+    logger.info("Testing if adding and iterating elements is implemented correctly");
+    int counter = 0;
+    for (int i : testList) {
+      counter++;
+    }
+    assertEquals(5, counter);
+  }
 
-	@Test
-	void testSize(){
-		logger.info("Testing if size() method is implemented correctly");
-		assertEquals(5, testList.size());
-	}
+  @Test
+  void testSize() {
+    logger.info("Testing if size() method is implemented correctly");
+    assertEquals(5, testList.size());
+  }
 
-	@Test
-	void testFilterAnonymousClass(){
-		logger.info("Testing the filter possibilities by filtering for all elements greater 2");
-		SimpleList<Integer> result = testList.filter(new SimpleFilter<Integer>() {
-			@Override
-			public boolean include(Integer item) {
-				int current = item;
-				return current > 2;
-			}
-		});
+  @Test
+  void testFilterAnonymousClass() {
+    logger.info("Testing the filter possibilities by filtering for all elements greater 2");
+    SimpleList<Integer> result = testList.filter(new SimpleFilter<Integer>() {
+      @Override
+      public boolean include(Integer item) {
+        int current = item;
+        return current > 2;
+      }
+    });
 
-		for(int o : result){
-			assertTrue(o > 2);
-		}
-	}
+    for (int o : result) {
+      assertTrue(o > 2);
+    }
+  }
 
-	@Test
-	void testFilterLambda(){
-		logger.info("Testing the filter possibilities by filtering for all elements which are dividable by 2");
-		SimpleList<Integer> result = testList.filter(o ->  o % 2 == 0);
-		for(int o : result){
-			assertEquals(0, o % 2);
-		}
-	}
+  @Test
+  void testFilterLambda() {
+    logger.info("Testing the filter possibilities by filtering for all elements which are dividable by 2");
+    SimpleList<Integer> result = testList.filter(o -> o % 2 == 0);
+    for (int o : result) {
+      assertEquals(0, o % 2);
+    }
+  }
+
+  @Test
+  void testMap() {
+    logger.info("Testing the default map function by converting all elements to strings");
+    //TODO
+  }
 }
